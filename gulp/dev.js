b70265b8c2a28f18e8129b3d65d046307193bc88
Kabuto43 <worldofwarcrafttop@gmail.com>
2024-07-07 16:34:31 +0300

    fix html:dev -> changed() added {hasChanged}

--- gulp/dev.js
+++ gulp/dev.js
@@ -52,7 +52,7 @@ gulp.task('clean:dev', function (done) {
 gulp.task('html:dev', function () {
     return gulp
         .src(['./src/html/**/*.html', '!./src/html/blocks/*.html'])
-        .pipe(changed('./build/'))
+        .pipe(changed('./build/', { hasChanged: changed.compareContents }))
         .pipe(plumber(plumberNotify('HTML')))
         .pipe(fileInclude(fileIncludeSettings))
         .pipe(gulp.dest('./build/'))
